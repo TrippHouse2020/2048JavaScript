4076bc10b3ff3a770ec162795c377a33a6bbcea9
TrippHouse2020 <132162481+TrippHouse2020@users.noreply.github.com>
2023-05-02 20:25:46 -0400

    Add files via upload

---
+++ 2048 Javascript/script.js
@@ -0,0 +1,212 @@
+let board;
+let score = 0;
+let rows = 4;
+let columns = 4;
+
+// Initialize the game board
+function setGame() {
+
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            let num = board[r][c];
+            updateTile(tile, num);
+            document.getElementById("board").append(tile);
+        }
+    }
+
+    setTwo();
+    setTwo();
+}
+
+// Update the intertext, color, and class name of the tile
+function updateTile(tile, num) {
+
+    tile.innerText = "";
+    tile.classList.value = "";
+    tile.classList.add("tile");
+
+    if (num > 0) {
+        tile.innerText = num.toString();
+        if (num <= 4096) {
+            tile.classList.add("x" + num.toString());
+        }
+        else {
+            tile.classList.add("x8192");
+        }
+    }
+}
+
+document.addEventListener('keyup', (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+        setTwo();
+    }
+    else if (e.code == "ArrowRight") {
+        slideRight();
+        setTwo();
+    }
+    else if (e.code == "ArrowUp") {
+        slideUp();
+        setTwo();
+    }
+    else if (e.code == "ArrowDown") {
+        slideDown();
+        setTwo();
+    }
+    document.getElementById("score").innerText = score;
+})
+
+function filterZero(row) {
+    return row.filter(num => num != 0);
+}
+
+function slide(row) {
+    row = filterZero(row);
+    for (let i = 0; i < row.length - 1; i++) {
+        if (row[i] == row[i + 1]) {
+            row[i] *= 2;
+            row[i + 1] = 0;
+            score += row[i];
+        }
+    }
+    row = filterZero(row);
+    while (row.length < columns) {
+        row.push(0);
+    }
+
+    return row;
+}
+
+// Slide all tiles to the left and merge adjacent tiles if possible
+function slideLeft() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row = slide(row);
+        board[r] = row;
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+// Slide all tiles to the right and merge adjacent tiles if possible
+function slideRight() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row.reverse();
+        row = slide(row);
+        board[r] = row.reverse();
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+// Slide all tiles up and merge adjacent tiles if possible
+function slideUp() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row = slide(row);
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+// Slide all tiles down and merge adjacent tiles if possible
+function slideDown() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row.reverse();
+        row = slide(row);
+        row.reverse();
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+// This function creates a tile with value of 2 at a random point on the board.
+function setTwo() {
+    if (!hasEmptyTile()) {
+
+        return;
+    }
+
+    let found = false;
+    while (!found) {
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        if (board[r][c] == 0) {
+            board[r][c] = 2;
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            tile.innerText = "2";
+            tile.classList.add("x2");
+            found = true;
+        }
+    }
+}
+
+// Keep track of empty cells
+function hasEmptyTile() {
+    let count = 0;
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            if (board[r][c] == 0) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+// Code for timer
+let startTime, timerIntervalId; 
+
+function startTimer() {
+  const timerElement = document.querySelector('#timer');
+  timerElement.innerText = '0:00'; // Reset timer to 0 when user starts typing
+  startTime = new Date();
+  timerIntervalId = setInterval(() => {
+    timerElement.innerText = getTimerTime();
+  }, 1000);
+}
+
+function getTimerTime() {
+  const elapsedTime = Math.floor((new Date() - startTime) / 1000);
+  const minutes = Math.floor(elapsedTime / 60).toString().padStart(2, '0');
+  const seconds = (elapsedTime % 60).toString().padStart(2, '0');
+  return `${minutes}:${seconds}`;
+}
+
+document.addEventListener("keydown", function(event) {
+  const timerElement = document.querySelector('#timer');
+  if (event.key === "ArrowUp" || event.key === "ArrowDown" || event.key === "ArrowLeft" || event.key === "ArrowRight") {
+    // Start the timer if it hasn't already started
+    if (!timerIntervalId) {
+      startTimer();
+    }
+  }
+});
+
